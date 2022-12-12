a1d710e21585298e7dacff30096626f02fc63336
UchechiOre <ucheore2001@gmail.com>
2022-12-12 14:28:04 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like a multilingual puzzle");
+	return (0);
+
+}
