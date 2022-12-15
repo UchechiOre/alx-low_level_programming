418c0fb311f1a9d63de31e29390e2f68e9c17e09
UchechiOre <ucheore2001@gmail.com>
2022-12-15 00:48:42 -0800

    times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,15 +17,14 @@ void times_table(void)
 			_putchar(' ');
 
 			prod = num + mult;
-			
-			if (prod <= 9)
-				_putchar (' ');
-			else 
-				_putchar ((prod / 10); + '0');
 
+			if (prod <= 9)
+				_putchar(' ');
+			else 
+				_putchar((prod / 10) + '0');
+			
 			_putchar((prod % 10) + '0');
 		}
 		-putchar('\n');
 	}
 }
-
