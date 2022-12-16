16205928fe9b0540ef4cfd3fe5a04c23bedf6337
UchechiOre <ucheore2001@gmail.com>
2022-12-16 13:22:01 -0800

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -17,9 +17,9 @@ void more_numbers(void)
 	{
 	if (y > 9)
 	{
-	_putchar((y / 10) +'0');
+	_putchar((y / 10) + '0');
 	}
-	putchar((y % 10) + '0');
+	_putchar((y % 10) + '0');
 	}
 	_putchar('\n');
 	}
